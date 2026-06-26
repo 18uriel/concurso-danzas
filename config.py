2801eff45940c789be2a0788e76818c08a24744f
Uriel <urielrojaschurquipaquispe@gmail.com>
2026-06-26 14:47:58 -0500

    Configurar variables de entorno para Render

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
@@ -6,8 +7,10 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''  # Cambiar por tu contraseña
-    MYSQL_DB = 'concurso_danzas'
+    # Configuración para Render - Usa las variables de entorno
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'concurso_danzas')
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
     MYSQL_CURSORCLASS = 'DictCursor'
